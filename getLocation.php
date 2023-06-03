231911ab917b834326e1fb829c8d12fe3a69bbc1
obiswas <omjit.biswas95@gmail.com>
2023-06-03 18:06:04 +0530

    seven commit

--- getLocation.php
+++ getLocation.php
@@ -2,7 +2,7 @@
 //if latitude and longitude are submitted
 if(!empty($_POST['latitude']) && !empty($_POST['longitude'])){
     //send request and receive json data by latitude and longitude
-    $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($_POST['latitude']).','.trim($_POST['longitude']).'&sensor=false';
+   echo $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($_POST['latitude']).','.trim($_POST['longitude']).'&sensor=false&key=AIzaSyAMUFmS_rS9t7swuWQjBeX2wTYSC4MVDsY';
     $json = @file_get_contents($url);
     $data = json_decode($json);
     $status = $data->status;
